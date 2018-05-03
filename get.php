5f1eed78c0914631eded2a289693f439d7d0faec
Hans-Joachim Kliemeck <git@kliemeck.de>
2018-05-03 20:03:56 +0200

    corrected tab value, fixed date

--- get.php
+++ get.php
@@ -1,6 +1,6 @@
 <?php
 
-$begin    = new DateTime('2012-11-09');
+$begin    = new DateTime('2012-01-01');
 $end      = new DateTime();
 $interval = DateInterval::createFromDateString('1 day');
 $days     = new DatePeriod($begin, $interval, $end);
@@ -9,7 +9,7 @@ $reversedays = array_reverse(iterator_to_array($days));
 foreach ( $reversedays as $day ) {
 	$date = date_format($day, 'Y-m-d');
 
-	$content = file_get_contents("https://github.com/h0nIg?tab=contributions&from=".$date);
+	$content = file_get_contents("https://github.com/h0nIg?tab=overview&from=".$date);
 	file_put_contents("./output/".$date, $content);
 
 	// 2,5 sec
