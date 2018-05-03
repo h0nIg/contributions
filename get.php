e302241422265df36783d3d455ca1d79e4ad78c9
Hans-Joachim Kliemeck <git@kliemeck.de>
2018-05-03 22:09:18 +0200

    speed increase for php curl

--- get.php
+++ get.php
@@ -8,19 +8,22 @@ $reversedays = array_reverse(iterator_to_array($days));
 
 $cookie = '';
 
+$curl = curl_init();
 foreach ( $reversedays as $day ) {
 	$date = date_format($day, 'Y-m-d');
 
-	$curl = curl_init("https://github.com/h0nIg?tab=overview&from=".$date);
+	curl_setopt($curl, CURLOPT_URL, "https://github.com/h0nIg?tab=overview&from=".$date);
 	curl_setopt($curl, CURLOPT_COOKIE, $cookie);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 	$content = curl_exec($curl);
-	curl_close($curl);
+
+	$info = curl_getinfo($curl);
+	if ($info['http_code'] != '200') {
+		die($date." error ".$info['http_code']);
+	}
 
 	file_put_contents("./output/".$date, $content);
-
-	// 2,5 sec
-	usleep(2500000);
 }
 
+curl_close($curl);
